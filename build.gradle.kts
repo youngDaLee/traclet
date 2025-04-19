6b65489fc1f262abfbf2fa58f7e2e345dd850780
youngDaLee <ldy0956@gmail.com>
2025-04-19 16:54:35 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    kotlin("jvm") version "2.0.21" apply false
+}
+
+allprojects {
+    group = "com.github.youngDaLee"
+    version = "0.0.1"
+
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "traclet"
+
+include(
+    "traclet-core",
+    "traclet-spring"
+)
