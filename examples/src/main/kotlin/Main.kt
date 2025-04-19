84ac749e39865b3c61bc4aa3ed7bea2257e32d69
youngDaLee <ldy0956@gmail.com>
2025-04-19 17:52:07 +0900

    init example

--- examples/src/main/kotlin/Main.kt
+++ examples/src/main/kotlin/Main.kt
@@ -1,33 +1,31 @@
 package examples
 
 import traclet.core.Traclet
-import traclet.core.alert.Alerter
+import traclet.core.alert.ConsoleAlerter
 import traclet.core.trigger.model.TriggerRule
 import traclet.core.trigger.ThresholdTrigger
 
 fun main() {
-    println("🟢 Traclet 예제 시작")
+    default()
+}
 
-    // 1. 알림 핸들러 정의
-    val consoleAlerter = object : Alerter {
-        override fun report(eventKey: String) {
-            println("🚨 '$eventKey' 이벤트가 트리거 조건을 만족했습니다!")
-        }
-    }
+fun default() {
+    println("🟢 Traclet 예제 시작 : default")
 
-    // 2. 트리거 조건 설정 (동일 키 이벤트가 3번 발생 시 알림)
-    val trigger = ThresholdTrigger(threshold = 3)
-    val rule = TriggerRule(trigger, consoleAlerter)
-
-    // 3. Traclet 초기화
+    // 1. 트리거 조건 설정 (동일 키 이벤트가 3번 발생 시 알림)
+    val rule = TriggerRule(
+        trigger = ThresholdTrigger(threshold = 3),
+        alerter = ConsoleAlerter()
+    )
+    // 2. Traclet 초기화
     Traclet.init(
         triggerRules = listOf(rule)
     )
 
     // 4. 이벤트 수집 (3번째 호출 시 알림 발생)
     repeat(3) {
-        Traclet.record("login.failed")
+        Traclet.record("test.key")
     }
 
-    println("✅ Traclet 예제 종료")
+    println("✅ Traclet 예제 종료 : default")
 }
