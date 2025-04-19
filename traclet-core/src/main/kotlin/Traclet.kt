4d129e871174967fefa97c55953b3a135e43e28f
youngDaLee <ldy0956@gmail.com>
2025-04-19 17:35:22 +0900

    feat: 알림 객체 생성

--- traclet-core/src/main/kotlin/Traclet.kt
+++ traclet-core/src/main/kotlin/Traclet.kt
@@ -1,19 +1,47 @@
 package traclet.core
 
+import traclet.core.collector.Store
+import traclet.core.collector.model.EventMetric
+import traclet.core.trigger.model.TriggerRule
+
 object Traclet {
+    private var _store: Store? = null
+    val store: Store
+        get() = _store ?: error("Traclet is not initialized. Call Traclet.init(...) first.")
 
-    fun init() {
-        // Initialize the Traclet library
-        println("Traclet library initialized.")
+    private val rules = mutableListOf<TriggerRule>()
+    private var initialized = false
+
+    fun init(
+        store: Store = Store.default(),
+        triggerRules: List<TriggerRule> = emptyList()
+    ) {
+        if (initialized) error("Traclet can only be initialized once.")
+
+        _store = store
+        rules.addAll(triggerRules)
+        initialized = true
     }
 
-    fun start() {
-        // Start the Traclet library
-        println("Traclet library started.")
-    }
+    fun record(
+        key: String,
+        duration: Long? = null,
+        tags: Map<String, String> = emptyMap()
+    ) {
+        val metric = EventMetric(
+            key = key,
+            duration = duration,
+            tags = tags
+        )
 
-    fun stop() {
-        // Stop the Traclet library
-        println("Traclet library stopped.")
+        // 저장소 저장
+        store.save(metric)
+
+        // 알림 조건 확인
+        rules.forEach { rule ->
+            if (rule.trigger.isTrigger(metric, store.getEvents()[metric.key] ?: emptyList())) {
+                rule.alerter.report(metric.key)
+            }
+        }
     }
 }
